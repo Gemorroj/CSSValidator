d26b90b4f2a16c245a51a27bfdef52cc0469d601
Gemorroj <wapinet@gmail.com>
2017-01-14 10:52:02 +0300

    change license from bsd to lgpl 3.0

--- src/CSSValidator/Exception.php
+++ src/CSSValidator/Exception.php
@@ -1,19 +1,7 @@
 <?php
-/**
- * This file contains the base class for utilizing an instance of the
- * W3 CSS Validator.
- *
- * PHP versions 5
- *
- * @license  http://www.opensource.org/licenses/bsd-license.php BSD
- */
+
 namespace CSSValidator;
 
-/**
- * A simple class for utilizing the W3C CSS Validator service.
- *
- * @license  http://www.opensource.org/licenses/bsd-license.php BSD
- */
 class Exception extends \Exception
 {
     
