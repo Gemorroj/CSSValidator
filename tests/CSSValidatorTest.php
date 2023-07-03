a0768bd5247b189e951c2ad373b5ff0f2232fc94
Gemorroj <wapinet@mail.ru>
2023-07-03 20:49:50 +0300

    fix: tests

--- tests/CSSValidatorTest.php
+++ tests/CSSValidatorTest.php
@@ -59,29 +59,18 @@ class CSSValidatorTest extends TestCase
         self::assertSame(Options::PROFILE_CSS3, $result->getCssLevel());
 
         self::assertEmpty($result->getWarnings());
-        self::assertCount(2, $result->getErrors());
+        self::assertCount(1, $result->getErrors());
 
         $error1 = $result->getErrors()[0];
 
         self::assertSame('parse-error', $error1->getErrorType());
         self::assertNull($error1->getContext());
         self::assertSame('unrecognized', $error1->getErrorSubType());
-        self::assertSame('error-css-test!!!ing {%&kground: green;', $error1->getSkippedString());
+        self::assertSame('error-css-test!!!ing {%&kground: green', $error1->getSkippedString());
         self::assertSame('file://localhost/TextArea', $error1->getUri());
         self::assertSame(1, $error1->getLine());
         self::assertSame('Parse Error', $error1->getMessage());
         self::assertSame('generator.unrecognize', $error1->getType());
-
-        $error2 = $result->getErrors()[1];
-
-        self::assertSame('parse-error', $error2->getErrorType());
-        self::assertNull($error2->getContext());
-        self::assertSame('skippedString', $error2->getErrorSubType());
-        self::assertSame('}', $error2->getSkippedString());
-        self::assertSame('file://localhost/TextArea', $error2->getUri());
-        self::assertSame(1, $error2->getLine());
-        self::assertSame('Parse Error', $error2->getMessage());
-        self::assertSame('generator.unrecognize', $error2->getType());
     }
 
     public function testWarningCSSFragment(): void
